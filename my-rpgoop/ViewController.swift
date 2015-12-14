75d77a0ebe95b2c05c9a1c7d41479ceef496981a
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2015-12-14 16:46:33 -0500

    first commit of new project ... nothing completed yet

--- my-rpgoop/ViewController.swift
+++ my-rpgoop/ViewController.swift
@@ -4,7 +4,8 @@
 //
 //  Created by DANIEL OREILLY on 12/14/15.
 //  Copyright © 2015 DANIEL OREILLY. All rights reserved.
-//
+//  modelled on rpgoop
+
 
 import UIKit
 
@@ -12,12 +13,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
 
 
