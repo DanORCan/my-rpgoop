ea7ebd77e6849effb2771814b9e54eeacb010c0f
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2015-12-17 22:52:45 -0500

    soldier class added

---
+++ my-rpgoop/Soldier.swift
@@ -0,0 +1,35 @@
+//
+//  Soldier.swift
+//  my-rpgoop
+//
+//  Created by DANIEL OREILLY on 12/17/15.
+//  Copyright © 2015 DANIEL OREILLY. All rights reserved.
+//
+
+import Foundation
+
+class Soldier: Character {
+    private var _name = "Soldier"
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    private var _inventory: [String] = ["sword","knife","helmet"]  //basic inventory 
+    
+    var inventory: [String] {
+        return _inventory
+    }
+    
+    func addItemToInventory(item: String) {
+        _inventory.append(item)
+    }
+    
+    convenience init(name: String, hp: Int, attackPwr: Int) {
+        self.init(startingHp: hp, attackPwr: attackPwr)
+        _name = name
+    }
+    
+}
