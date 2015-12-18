d46e4b67aa360df31d5fdbde0083f18a5c72cfbf
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2015-12-17 21:29:21 -0500

    character class added

---
+++ my-rpgoop/Character.swift
@@ -0,0 +1,46 @@
+//
+//  Character.swift
+//  my-rpgoop
+//
+//  Created by DANIEL OREILLY on 12/17/15.
+//  Copyright © 2015 DANIEL OREILLY. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    private var _hp:  Int = 100 // hp health points
+    private var _attackPwr = 10 // attackPwr attack power
+    
+    var attackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
+    
+    var hp: Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var isAlive: Bool {
+        get {
+            if hp <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+    
+    init(startingHp: Int, attackPwr: Int) {
+        self._hp = startingHp
+        self._attackPwr = attackPwr
+    }
+    
+    func attemptAttack(attackPwr: Int) -> Bool {
+        self._hp -= attackPwr
+        return true
+    }
+}
